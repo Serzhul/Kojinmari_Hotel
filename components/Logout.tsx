3211ad64275bd7ecc877bf33a099c8f11fb3f4bd
Serzhul <wilshere917@gmail.com>
2023-08-25 15:26:08 +0900

    fix: 불필요 로직 삭제

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,5 +1,4 @@
 import { Logout as LogoutIcon } from 'tabler-icons-react'
-import ButtonIcon from './ButtonIcon'
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useState } from 'react'
